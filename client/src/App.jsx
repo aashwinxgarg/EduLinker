162cbf0cbab3ef063a2a751126190b19a76eab4f
Aashwin Garg <aashwinxgarg@gmail.com>
2024-11-22 17:58:07 +0530

    build login/ signup page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,14 @@
 import './App.css'
 import { Button } from './components/ui/button'
+import Login from './pages/login'
 
 function App() {
 
   return (
-    <Button>Let's Build LMS</Button>
+    <main>
+        <Login/>
+        
+    </main>
   )
 }
 
