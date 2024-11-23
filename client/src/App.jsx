88c14b9d0eaa956c0a6a7ce808ee129c0663d987
Aashwin Garg <aashwinxgarg@gmail.com>
2024-11-23 15:23:30 +0530

    setup react router dom

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,41 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './App.css'
 import Navbar from './components/Navbar'
 import { Button } from './components/ui/button'
 import Login from './pages/login'
 import HeroSection from './pages/student/HeroSection'
+import MainLayout from './layout/MainLayout'
+import Courses from './pages/student/Courses'
+
+const appRouter = createBrowserRouter([
+    {
+        path: "/",
+        element: <MainLayout />,
+        children: [
+            {
+                path: "/",
+                element: (
+                    <>
+                        <HeroSection />
+                        <Courses/>
+                    </>
+                )
+            },
+            {
+                path:"/login",
+                element:<Login/>
+            }
+        ]
+    }
+])
 
 function App() {
 
-  return (
-    <main>
-        <Navbar/>
-        <HeroSection/>
-        <Login/>
-        
-    </main>
-  )
+    return (
+        <main>
+            <RouterProvider router={appRouter}/>
+        </main>
+    )
 }
 
 export default App
