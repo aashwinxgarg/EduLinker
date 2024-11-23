baf53508db1a8241b97fb4b2da70e49da983bf69
Aashwin Garg <aashwinxgarg@gmail.com>
2024-11-23 12:48:49 +0530

    build navbar page

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,11 +1,14 @@
-import { School } from 'lucide-react'
+import { Menu, School } from 'lucide-react'
 import React from 'react'
 import { Button } from './ui/button';
 import { DropdownMenu, DropdownMenuContent, DropdownMenuGroup, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from './ui/dropdown-menu';
 import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar';
+import DarkMode from '@/DarkMode';
+import { Sheet, SheetClose, SheetContent, SheetFooter, SheetHeader, SheetTitle, SheetTrigger } from './ui/sheet';
+import { Separator } from '@radix-ui/react-dropdown-menu';
 
 const Navbar = () => {
-    const user = false;
+    const user = true;
     return (
         <div className='h-16 dark:bg-[#0a0a0a] bg-white border-b dark:border-b-gray-800 border-b-gray-200 fixed top-0 left-0 right-0 duration-300 z-10'>
             {/* Desktop */}
@@ -15,7 +18,7 @@ const Navbar = () => {
                     <h1 className='hidden md:block font-extrabold text-2xl'>edulinker</h1>
                 </div>
                 {/* User icons and dark mode icon  */}
-                <div>
+                <div className='flex items-center gap-8'>
                     {
                         user ? (
                             <DropdownMenu>
@@ -46,17 +49,56 @@ const Navbar = () => {
                                 </DropdownMenuContent>
                             </DropdownMenu>
                         ) : (
-                            <div>
+                            <div className='flex items-center gap-2'>
                                 <Button variant="outline">Login</Button>
                                 <Button>Signup</Button>
                             </div>
                         )
                     }
-                    <DarkMode/>
+                    <DarkMode />
                 </div>
+
+            </div>
+            {/* Mobile Device  */}
+            <div className='flex md:hidden items-center justify-between h-full px-4 '>
+                <h1 className='font-extrabold text-2xl'>edulinker</h1>
+                <MobileNavbar />
             </div>
         </div>
     )
 }
 
 export default Navbar
+
+const MobileNavbar = () => {
+    const role="instructor"
+    return (
+        <Sheet>
+            <SheetTrigger asChild>
+                <Button size='icon' className='rounded-full bg-gray-200 hover:bg-gray-200' variant="outline"><Menu /></Button>
+            </SheetTrigger>
+            <SheetContent className='flex flex-col'>
+                <SheetHeader className='flex flex-row items-center justify-between mt-2'>
+                    <SheetTitle>edulinker</SheetTitle>
+                    <DarkMode/>
+                </SheetHeader>
+                <Separator/>
+                <nav className='flex flex-col space-y-4'>
+                    <span>My Learning</span>
+                    <span>Edit Profile</span>
+                    <span>Log out</span>
+                </nav>
+                {
+                    role === "instructor" && (
+                        <SheetFooter>
+                        <SheetClose asChild>
+                            <Button type="submit">Dashboard</Button>
+                        </SheetClose>
+                    </SheetFooter>
+                    )
+                }
+                
+            </SheetContent>
+        </Sheet>
+    )
+}
