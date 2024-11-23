f2eb5785db964b7fed95b391adfcf20c3eed85f9
Aashwin Garg <aashwinxgarg@gmail.com>
2024-11-23 14:45:28 +0530

    build hero section page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,12 +2,14 @@ import './App.css'
 import Navbar from './components/Navbar'
 import { Button } from './components/ui/button'
 import Login from './pages/login'
+import HeroSection from './pages/student/HeroSection'
 
 function App() {
 
   return (
     <main>
         <Navbar/>
+        <HeroSection/>
         <Login/>
         
     </main>
