94a15a533debe110776e9424945032a7ae3ce40d
Aashwin Garg <aashwinxgarg@gmail.com>
2024-11-26 20:32:10 +0530

    build add course page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,6 +11,7 @@ import Profile from './pages/student/Profile'
 import Sidebar from './pages/admin/Sidebar'
 import Dashboard from './pages/admin/Dashboard'
 import CourseTable from './pages/admin/course/CourseTable'
+import AddCourse from './pages/admin/course/AddCourse'
 
 const appRouter = createBrowserRouter([
     {
@@ -49,10 +50,13 @@ const appRouter = createBrowserRouter([
                         element:<Dashboard/>
                     },
                     {
-                        path:"courses",
+                        path:"course",
                         element:<CourseTable/>
                     },
-
+                    {
+                        path:"course/create",
+                        element:<AddCourse/>
+                    },
                 ]
             }
         ],
